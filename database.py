dface038c3226b994c11db5fc01e3663e32576d9
hashemUOP <167630882+hashemUOP@users.noreply.github.com>
2025-12-26 15:47:51 +0300

    insert search in table

---
+++ database.py
@@ -0,0 +1,36 @@
+table = [
+    # [
+    # {"ID":""},{"Author":""},{"Genre":""},{"Title":""},{"Purchase Price JOD":""},{"Purchase Date":""},
+    # ],table structure
+]
+
+
+def insert(id, author, genre, title, purchase_price, purchase_date):
+    table.append(
+        [
+            {"ID": id},
+            {"Author": author},
+            {"Genre": genre},
+            {"Title": title},
+            {"Purchase Price JOD": purchase_price},
+            {"Purchase Date": purchase_date},
+        ]
+    )  # this adds data of the book as list of dictionaries to table at its last row
+
+
+# example of inserting data to table
+insert('0', 'charles', 'horror', 'five trees', '10', '12/26/2026')
+
+
+def search(title):
+    found = False
+    for row in table:
+        for item in row:
+            if item.get("Title") == title:
+                found = True
+                print(row)
+
+    if not found:
+        print("Book not found.")
+
+search(input("Enter Title: "))
